e3ff948e90f5b35af5fcfe63ed5745805eff58ce
russomp <russomp@usc.edu>
2018-09-15 03:33:05 -0400

    feat: Make call to action button active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 (() => {
+  /* Event listeners for navigating between section */
+  const c2aBtnElem = document.getElementById('c2a-btn');
+  c2aBtnElem.addEventListener('click', evt => {
+    window.location.href = '#section-2';
+  });
+
   const toNextSectionElems = document.querySelectorAll(
     '.to-next-section__icon'
   );
   toNextSectionElems.forEach((elem, i) => {
     elem.addEventListener('click', evt => {
-      console.log(i);
       window.location.href = `#section-${i + 2}`;
     });
   });
