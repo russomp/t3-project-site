48260a1001c3945ebf185c64175daf033670971e
russomp <russomp@usc.edu>
2018-09-24 16:34:22 -0400

    fix: Work on fixing file table display issues

--- PR/index.php
+++ PR/index.php
@@ -71,11 +71,13 @@
       </div>
     </div>
   </nav>
-  <section id="project-details" class="">
+  <section id="project-details" class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container">
         <h1 class="title">
           No documents found
         </h1>
-        <table class="sortable">
+        <table class="table sortable">
           <thead>
             <tr>
               <th>Filename</th>
@@ -86,108 +88,112 @@
           </thead>
           <tbody>
           <?php
-            // Opens directory
-            $myDirectory=opendir(".");
-            
-            // Gets each entry
-            while($entryName=readdir($myDirectory)) {
-              $dirArray[]=$entryName;
-            }
-            
-            // Finds extensions of files
-            function findexts ($filename) {
-              $filename=strtolower($filename);
-              $exts=explode("[/\\.]", $filename);
-              $n=count($exts)-1;
-              $exts=$exts[$n];
-              return $exts;
-            }
-            
-            // Closes directory
-            closedir($myDirectory);
-            
-            // Counts elements in array
-            $indexCount=count($dirArray);
-            
-            // Sorts files
-            sort($dirArray);
-            
-            // Loops through the array of files
-            for($index=0; $index < $indexCount; $index++) {
-            
-              // Allows ./?hidden to show hidden files
-              if($_SERVER['QUERY_STRING']=="hidden")
-              {$hide="";
-              $ahref="./";
-              $atext="Hide";}
-              else
-              {$hide=".";
-              $ahref="./?hidden";
-              $atext="Show";}
-              if(substr("$dirArray[$index]", 0, 1) != $hide) {
-              
-              // Gets File Names
-              $name=$dirArray[$index];
-              $namehref=$dirArray[$index];
-              
-              // Gets Extensions 
-              $extn=findexts($dirArray[$index]); 
-              
-              // Gets file size 
-              $size=number_format(filesize($dirArray[$index]));
-              
-              // Gets Date Modified Data
-              $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-              $timekey=date("YmdHis", filemtime($dirArray[$index]));
-              
-              // Prettifies File Types, add more to suit your needs.
-              switch ($extn){
-                case "png": $extn="PNG Image"; break;
-                case "jpg": $extn="JPEG Image"; break;
-                case "svg": $extn="SVG Image"; break;
-                case "gif": $extn="GIF Image"; break;
-                case "ico": $extn="Windows Icon"; break;
-                
-                case "txt": $extn="Text File"; break;
-                case "log": $extn="Log File"; break;
-                case "htm": $extn="HTML File"; break;
-                case "php": $extn="PHP Script"; break;
-                case "js": $extn="Javascript"; break;
-                case "css": $extn="Stylesheet"; break;
-                case "pdf": $extn="PDF Document"; break;
-                
-                case "zip": $extn="ZIP Archive"; break;
-                case "bak": $extn="Backup File"; break;
-                
-                default: $extn=strtoupper($extn)." File"; break;
-              }
-              
-              // Separates directories
-              if(is_dir($dirArray[$index])) {
-                $extn="&lt;Directory&gt;"; 
-                $size="&lt;Directory&gt;"; 
-                $class="dir";
-              } else {
-                $class="file";
-              }
-              
-              // Cleans up . and .. directories 
-              if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
-              if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-              
-              // Print 'em
-              print("
-              <tr class='$class'>
-                <td><a href='./$namehref'>$name</a></td>
-                <td><a href='./$namehref'>$extn</a></td>
-                <td><a href='./$namehref'>$size</a></td>
-                <td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
-              </tr>");
-              }
-            }
-          ?>
+  // Opens directory
+  $myDirectory=opendir(".");
+  
+  // Gets each entry
+  while($entryName=readdir($myDirectory)) {
+    $dirArray[]=$entryName;
+  }
+  
+  // Finds extensions of files
+  function findexts ($filename) {
+    $filename=strtolower($filename);
+    $exts=explode("[/\\.]", $filename);
+    $n=count($exts)-1;
+    $exts=$exts[$n];
+    return $exts;
+  }
+  
+  // Closes directory
+  closedir($myDirectory);
+  
+  // Counts elements in array
+  $indexCount=count($dirArray);
+  
+  // Sorts files
+  sort($dirArray);
+  
+  // Loops through the array of files
+  for($index=0; $index < $indexCount; $index++) {
+  
+    // Allows ./?hidden to show hidden files
+    if($_SERVER['QUERY_STRING']=="hidden") {
+      $hide="";
+      $ahref="./";
+      $atext="Hide";
+    } else {
+      $hide=".";
+      $ahref="./?hidden";
+      $atext="Show";
+    }
+    
+    if(substr("$dirArray[$index]", 0, 1) != $hide) {
+    
+      // Gets File Names
+      $name=$dirArray[$index];
+      $namehref=$dirArray[$index];
+      
+      // Gets Extensions 
+      $extn=findexts($dirArray[$index]); 
+      
+      // Gets file size 
+      $size=number_format(filesize($dirArray[$index]));
+      
+      // Gets Date Modified Data
+      $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
+      $timekey=date("YmdHis", filemtime($dirArray[$index]));
+      
+      // Prettifies File Types, add more to suit your needs.
+      echo($extn);
+      switch ($extn){
+        case "php": $extn="php"; break;
+
+        case "png": $extn="PNG Image"; break;
+        case "jpg": $extn="JPEG Image"; break;
+        case "svg": $extn="SVG Image"; break;
+        case "gif": $extn="GIF Image"; break;
+        
+        case "txt": $extn="Text File"; break;
+        case "log": $extn="Log File"; break;
+        case "pdf": $extn="PDF Document"; break;
+        
+        case "zip": $extn="ZIP Archive"; break;
+        case "mpp": $extn="Microsoft Project Plan"; break;
+        
+        default: $extn=strtoupper($extn)." File"; break;
+      }
+      
+      // Separates directories
+      if(is_dir($dirArray[$index])) {
+        $extn="&lt;Directory&gt;"; 
+        $size="&lt;Directory&gt;"; 
+        $class="dir";
+      } else {
+        $class="file";
+      }
+      
+      // Cleans up . and .. directories 
+      if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;";}
+      if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
+
+      // Print 'em
+      if($extn!="php") {
+        print("
+        <tr class='$class'>
+          <td><a href='./$namehref'>$name</a></td>
+          <td>$extn</td>
+          <td>$size</td>
+          <td sorttable_customkey='$timekey'>$modtime</td>
+        </tr>");
+      }
+    }
+  }
+?>
           </tbody>
         </table>
+      </div>
+    </div>
   </section>
 </body>
 
